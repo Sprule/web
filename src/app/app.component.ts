e65c550dbc8788df1a76f74874fd6eed0233d9c0
Luke Chatton <chattonluke@gmail.com>
2018-02-01 09:16:04 -0800

    implement apollo client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 @Component({
   selector: 'app-root',
   template: `
-  <h1>Universal Demo using Angular and Angular CLI</h1>
-  <a routerLink="/">Home</a>
-  <a routerLink="/lazy">Lazy</a>
-  <a routerLink="/lazy/nested">Lazy_Nested</a>
-  <router-outlet></router-outlet>
+    <h1>Universal Demo using Angular and Angular CLI</h1>
+    <a routerLink="/">Home</a>
+    <a routerLink="/lazy">Lazy</a>
+    <a routerLink="/lazy/nested">Lazy_Nested</a>
+    <router-outlet></router-outlet>
   `,
   styles: []
 })
 export class AppComponent {
-
+    constructor(
+        apollo: Apollo,
+        httpLink: HttpLink
+    ) {
+        apollo.create({
+            link: httpLink.create({ uri: 'http://localhost:3000/graphql' }),
+            cache: new InMemoryCache()
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule } from 'apollo-angular';
+import { HttpLinkModule } from 'apollo-angular-link-http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -16,7 +19,10 @@ import { HomeComponent } from './home/home.component';
       { path: '', component: HomeComponent, pathMatch: 'full'},
       { path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'},
       { path: 'lazy/nested', loadChildren: './lazy/lazy.module#LazyModule'}
-    ])
+      ]),
+      HttpClientModule, // provides HttpClient for HttpLink
+      ApolloModule,
+      HttpLinkModule
   ],
   providers: [],
   bootstrap: [AppComponent]
