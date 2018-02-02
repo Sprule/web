b9748945f0afe26091766a9ddcfc73cece5b0d8e
Luke Chatton <chattonluke@gmail.com>
2018-02-02 08:18:08 -0800

    remove demo html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,6 @@ import { AuthService } from './services/auth.service';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Universal Demo using Angular and Angular CLI</h1>
-    <a routerLink="/">Home</a>
-    <a routerLink="/lazy">Lazy</a>
-    <a routerLink="/lazy/nested">Lazy_Nested</a>
     <router-outlet></router-outlet>
   `,
   styles: []
