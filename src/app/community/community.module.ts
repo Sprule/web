6b410cc9be2e5d30c75292a1c498aabf4fdfbf53
Luke Chatton <chattonluke@gmail.com>
2018-02-06 10:03:49 -0800

    component scaffolding

--- src/app/community/community.module.ts
+++ src/app/community/community.module.ts
@@ -5,14 +5,23 @@ import { HomeComponent } from './home/home.component'
 import { CommunityGuard } from '../guards/community.guard';
 import { MatMenuModule, MatButtonModule, MatToolbarModule, MatIconModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { TopicIndexComponent } from './forum/topic-index/topic-index.component';
+import { SidebarComponent } from './forum/sidebar/sidebar.component';
+import { PostComponent } from './forum/post/post.component';
+import { TopicComponent } from './forum/topic/topic.component';
+import { CategoryComponent } from './forum/category/category.component';
 
 @NgModule({
-    declarations: [HomeComponent, NavbarComponent],
+    declarations: [HomeComponent, NavbarComponent, TopicIndexComponent, SidebarComponent, PostComponent, TopicComponent, CategoryComponent],
     imports: [
         RouterModule.forChild([
             {
                 path: '', component: NavbarComponent, canActivate: [CommunityGuard], children: [
-                    { path: '', component: HomeComponent, pathMatch: 'full' }
+                    { path: '', component: HomeComponent, pathMatch: 'full' },
+                    {
+                        path: 'forums', component: SidebarComponent, children: [
+                            { path: '', component: TopicIndexComponent, pathMatch: 'full' }
+                    ]}
                 ]
             }
         ]),
---
+++ src/app/community/forum/topic/topic.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topic',
+  template: `
+    <p>
+      topic works!
+    </p>
+  `,
+  styles: []
+})
+export class TopicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
