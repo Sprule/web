1ac4c41562d255225cac35aa5008908c9bb20b7a
Luke Chatton <chattonluke@gmail.com>
2018-02-07 17:06:20 -0800

    admin sidenav

--- src/app/community/admin/admin.module.ts
+++ src/app/community/admin/admin.module.ts
@@ -1,22 +1,29 @@
+import { AdminSidebarComponent } from './sidebar/sidebar.component';
 import { CommunityAdminGuard } from './../../guards/community-admin.guard';
 import { AdminHomeComponent } from './home/home.component';
 import { NgModule, Component } from '@angular/core'
 import { RouterModule } from '@angular/router';
 import { CommunityGuard } from '../../guards/community.guard';
-import { MatMenuModule, MatButtonModule, MatToolbarModule, MatIconModule } from '@angular/material';
+import { MatMenuModule, MatButtonModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { CommonModule } from '@angular/common';
 import { NavbarModule } from '../../navbar/navbar.module';
+import { AdminCategoriesComponent } from './categories/categories.component';
 
 @NgModule({
-    declarations: [AdminHomeComponent],
+    declarations: [AdminHomeComponent, AdminSidebarComponent, AdminCategoriesComponent],
     imports: [
         CommonModule,
         NavbarModule,
         RouterModule.forChild([
-            { path: '', component: AdminHomeComponent }
+            {
+                path: '', component: AdminSidebarComponent, children: [
+                    { path: '', component: AdminHomeComponent },
+                    { path: 'categories', component: AdminCategoriesComponent }
+                ]
+            }
         ]),
-        MatMenuModule, MatButtonModule, MatToolbarModule, MatIconModule,
+        MatMenuModule, MatButtonModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule,
         FlexLayoutModule
     ]
 })
---
+++ src/app/community/admin/categories/categories.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-admin-categories',
+    templateUrl: './categories.component.html',
+    styleUrls: ['./categories.component.scss']
+})
+export class AdminCategoriesComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+        
+    }
+
+}
